c304b37ffd1636ef87594713bbae037daca38003
Aref Chucri <arefchucri@MacBook-Air-de-Aref.local>
2025-01-07 14:27:48 -0300

    alteracoes na hora de adicionar informacoes nos campos , toda vez que adicionar algo e a data ele vai gerar um id e as informacoes inseridas

---
+++ src/app/appointment-list/appointment-list.component.ts
@@ -0,0 +1,40 @@
+import { Appointment } from './../models/appointment';
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-appointment-list',
+  templateUrl: './appointment-list.component.html',
+  styleUrls: ['./appointment-list.component.css']
+})
+export class AppointmentListComponent {
+
+  newAppointmentTitle : string = "";
+  newAppointmentDate : Date = new Date();
+
+appointments : Appointment [] = []
+
+addAppontments () {
+
+if (this.newAppointmentTitle.trim().length && this.newAppointmentDate){
+  let newAppointment : Appointment = {
+    id: Date.now(),
+    title : this.newAppointmentTitle,
+    date: this.newAppointmentDate,
+
+
+  }
+
+  this.appointments.push(newAppointment)
+
+  this.newAppointmentTitle = "";
+  this.newAppointmentDate = new Date();
+
+  alert(this.appointments.length)
+}
+
+
+
+}
+
+}
---
+++ src/app/models/appointment.ts
@@ -0,0 +1,8 @@
+import { Data } from "@angular/router"
+
+export interface Appointment {
+
+  id: number
+  date : Data
+  title: string
+}
